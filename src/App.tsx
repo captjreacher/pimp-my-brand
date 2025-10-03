3c09adc84f5256b26c28c20b8ba431942372affc
Mike Robinson <mike@mikerobinson.co.nz>
2025-10-04 02:48:52 +1300

    Add uploads management page for files and text

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Gallery from "./pages/Gallery";
 import TemplatePreview from "./pages/TemplatePreview";
 import CreateBrand from "./pages/CreateBrand";
 import BrandView from "./pages/BrandView";
+import Uploads from "./pages/Uploads";
 import Pricing from "./pages/Pricing";
 import NotFound from "./pages/NotFound";
 
@@ -30,6 +31,7 @@ const App = () => (
           <Route path="/pricing" element={<Pricing />} />
           <Route path="/create" element={<CreateBrand />} />
           <Route path="/brand/:id" element={<BrandView />} />
+          <Route path="/uploads" element={<Uploads />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
